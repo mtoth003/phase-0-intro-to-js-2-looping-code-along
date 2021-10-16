e83e449956e772dc8ea11ff99f80b170856165e8
Michael Toth <mtoth003@gmail.com>
2021-10-15 21:52:19 -0400

    updated countdown

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// Code your solutions in this file
-/*for (let age = 30; age < 40; age++) {
-    console.log(`I'm ${age} years old. Happy birthday to me!`);
-    debugger;
-}*/
+//  Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
 
-/*const gifts = ["teddy bear", "drone", "doll"];
+// const gifts = ["teddy bear", "drone", "doll"];
 
-function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    }
-    return gifts;
-}
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
 
-wrapGifts(gifts);*/
+// wrapGifts(gifts);
 
 function writeCards(names, event) {
     let messages = []
@@ -24,9 +24,11 @@ function writeCards(names, event) {
 
 };
 
-function countDown(){
-    let x = 0;
-    while(x < 11) {
-        console.log(x++);
-    }
+writeCards(["John", "Sam", "Steve"], "Birthday");
+
+function countDown(i){
+    while(i > 0){
+        console.log(i);
+        i--;
+    };
 }
